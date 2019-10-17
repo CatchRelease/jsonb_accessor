135a6bc70775a1ed7a76673660b36627db8adf35
mpd <mpd@jesters-court.net>
2019-10-17 13:37:35 -0700

    Revert "Don't mixin to ActiveRecord::Base"
    This reverts commit c3bae1462e9d51569c978c6eea4624f613ff88b7.

--- lib/jsonb_accessor.rb
+++ lib/jsonb_accessor.rb
@@ -13,3 +13,8 @@ module JsonbAccessor
   extend ActiveSupport::Concern
   include Macro
 end
+
+ActiveSupport.on_load(:active_record) do
+  ActiveRecord::Base.send(:include, JsonbAccessor)
+  ActiveRecord::Base.send(:include, JsonbAccessor::QueryBuilder)
+end
